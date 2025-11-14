5411b7d138708a6793c30b40d1b3fa22e289fb8c
Gavin Hensley <gavinhensley@protonmail.com>
2025-11-14 14:18:21 -0500

    Add 3 courses for now

--- app/components/layout/home/contentTabs/CoursesTab.tsx
+++ app/components/layout/home/contentTabs/CoursesTab.tsx
@@ -22,13 +22,23 @@ export default function CoursesTab() {
   ]
 
   return (
-    <section className="w-full h-full flex flex-wrap justify-center gap-8 rounded-lg rounded-tl-none text-white">
+    <section 
+      className="w-full h-full flex flex-wrap justify-center gap-8 rounded-lg rounded-tl-none border
+      border-primary/50 p-8"
+    >
       {courses.map((course, index) => (
-        <div key={index}>
-          <h3>{course.title}</h3>
-          <p>{course.description}</p>
-          <p>Level: {course.level}</p>
-          <p>CPE Credits: {course.cpe}</p>
+        <div 
+          key={index}
+          className="w-sm border border-primary rounded-lg p-4 pb-0 flex flex-col bg-radial from-primary/20 to-primary/5
+            hover:brightness-125 transition-all duration-300 ease-in-out cursor-pointer relative gap-4 shadow-lg shadow-black/40"
+        >
+          <h3 className="text-center font-primary text-xl border-b border-secondary/50 pb-1 text-slate-200">{course.title}</h3>
+          <p className="text-slate-300">{course.description}</p>
+          <p className="text-slate-300 text-center p-2 border-b-0 border-primary border w-fit mx-auto rounded-t-lg">{course.level}</p>
+          <p className="text-slate-900 absolute -top-3 -right-3 bg-secondary text-sm p-1.5 rounded-2xl shadow-md
+            shadow-black/30 font-medium">
+            {course.cpe} CPEs
+          </p>
         </div>
       ))}
     </section>
--- app/components/layout/home/contentTabs/Tabs.tsx
+++ app/components/layout/home/contentTabs/Tabs.tsx
@@ -7,8 +7,8 @@ export default function Tabs() {
       {tabs.map((tab) => (
         <button 
           key={tab} 
-          className="px-4 py-2 rounded-t-lg transition-all text-lg text-slate-300 border-secondary/80 border 
-            border-b-primary cursor-pointer duration-300 ease-in-out hover:brightness-200 hover:text-white">
+          className="px-4 py-1 rounded-t-lg transition-all text-lg text-slate-300 border-secondary/70 border 
+            border-b-primary/50 cursor-pointer duration-300 ease-in-out hover:brightness-200 hover:text-white">
           {tab}
         </button>
       ))}
