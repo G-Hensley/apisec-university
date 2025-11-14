ef3f628f6556f28338b57e70014a03ec25f08e7c
Gavin Hensley <gavinhensley@protonmail.com>
2025-11-14 10:07:34 -0500

    Setup website and header

---
+++ app/components/layout/home/Hero.tsx
@@ -0,0 +1,12 @@
+export default function Hero() {
+  return (
+    <section className="flex flex-col items-center p-8 justify-center min-h-fit h-screen">
+      <header className="w-lg flex flex-col gap-2">
+        <h1 className="text-slate-300 text-6xl">Free, Real-World API Security Training</h1>
+        <h2 className="text-slate-400 text-2xl">
+          Free courses from industry experts—plus tools to secure your APIs in production
+        </h2>
+      </header>
+    </section>
+  );
+}
